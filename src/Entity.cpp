413a60f95129252bf7398a709b43c1b5fdb404a5
Ian Malcolm <ianmalcolm13@gmail.com>
2023-04-05 23:56:18 -0700

    Updated .gitignore to ignore .gch files + added Entity.cpp to src folder

---
+++ src/Entity.cpp
@@ -0,0 +1,40 @@
+#include "Entity.h"
+
+int Entity::getId() {
+   return id;
+}
+
+bool Entity::hasComponent(ComponentType type) {
+   return ((1<<type) & componentTypes) == (1<<type);
+}
+
+Component* Entity::getComponent(ComponentType type) {
+   if (hasComponent(type))
+      return componentPtrs[type];
+
+   return nullptr;
+}
+
+void Entity::removeComponent(ComponentType type) {
+   if (((1<<type) & componentTypes) != (1<<type)) {
+      return;
+   }
+   
+   if (componentPtrs[type] < (Component*)this || (char*)componentPtrs[type] > (char*)this+bytesUsed) {
+      free(componentPtrs[type]);
+   }
+   
+   componentTypes = componentTypes ^ (1<<type);
+   componentPtrs[type] = nullptr;
+}
+
+void Entity::reset() {
+   for (int i=0; i<NUM_COMPONENT_TYPES; i++) {
+      removeComponent((ComponentType) i);
+   }
+   componentTypes = 0;
+}
+
+Entity::~Entity() {
+   reset();
+}
