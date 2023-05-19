442deb9ded07761f835a76edd9376d9f54ae8e11
Ian Malcolm <ianmalcolm13@gmail.com>
2023-05-18 23:44:52 -0700

    Made starting an automove cancel look mode

--- src/PlayerManager.cpp
+++ src/PlayerManager.cpp
@@ -141,6 +141,11 @@ void PlayerManager::clearAutoAct() {
 }
 
 void PlayerManager::startAutoMove() {
+   if (inputState == PLAYER_INPUT_LOOK) {
+      map->stopLooking();
+      inputState = PLAYER_INPUT_MOVE;
+   }
+
    autoMoveRoute = map->getRouteToMouseTile();
    autoMoveRoute.resetProgress();
    autoActing = true;
